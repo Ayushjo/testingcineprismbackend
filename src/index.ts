abda1099bb12a0380533418972b58c7b474f7416
AyushJo <ayushs24101@iiitnr.edu.in>
2025-08-05 21:45:34 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,17 @@ export default client;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://testingcineprism.vercel.app"],
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "https://testingcineprism.vercel.app",
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
