9e613850ebf7cd47a6b90af9d3342992e860cb9b
AyushJo <ayushs24101@iiitnr.edu.in>
2025-08-14 12:58:35 +0530

    build: update dist files

--- dist/routes/adminRoutes.js
+++ dist/routes/adminRoutes.js
@@ -11,5 +11,5 @@ const router = (0, express_1.Router)();
 router.route("/add-poster").post(extractUser_1.extractUserDetails, multer_1.default.single("file"), adminController_1.uploadPoster);
 router.route("/create-post").post(extractUser_1.extractUserDetails, adminController_1.createPost);
 router.route("/upload-images").post(extractUser_1.extractUserDetails, multer_1.default.array("files", 10), adminController_1.uploadImages);
-router.route("/fetch-posts").get(adminController_1.fetchAllPost);
+router.route("/fetch-posts").post(adminController_1.fetchAllPost);
 exports.default = router;
--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -13,5 +13,5 @@ const router = Router();
 router.route("/add-poster").post(extractUserDetails,uploadFile.single("file"), uploadPoster);
 router.route("/create-post").post(extractUserDetails,createPost);
 router.route("/upload-images").post(extractUserDetails,uploadFile.array("files",10), uploadImages);
-router.route("/fetch-posts").get(fetchAllPost);
+router.route("/fetch-posts").post(fetchAllPost);
 export default router;
